76abf4a67795a7a2429a9e3dc62272ab9a4104d5
Manuel Rodriguez <manrodri92@gmail.com>
2019-07-30 04:10:26 +0300

    Added: Session 2 JS cardio

---
+++ Javascript Cardio/session2.js
@@ -0,0 +1,50 @@
+// CHALLENGE 1: LONGEST WORD
+// Return the longest word of a string
+// ex. longestWord('Hi there, my name is Brad') === 'there,'
+
+function longestWord(sen) {
+  // Create filter array
+  const wordArray = sen.toLowerCase().match(/[a-z0-9]+/g);
+  //   Sort by lenght
+  const sorted = wordArray.sort((a, b) => b.length - a.length);
+  //   If there are multiple words,put in array
+  const longestWordArray = sorted.filter(
+    word => word.length === sorted[0].length
+  );
+  console.log(longestWordArray);
+  // SOLUTION 1 - Return a single longest word
+  // SOLUTION 2 - Return an array and include multiple words if they have the same length
+  // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
+}
+
+// CHALLENGE 2: ARRAY CHUNKING
+// Split an array into chunked arrays of a specific length
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
+
+function chunkArray(arr, len) {}
+
+// CHALLENGE 3: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+
+function flattenArray(arrays) {}
+
+// CHALLENGE 4: ANAGRAM
+// Return true if anagram and false if not
+// ex. 'elbow' === 'below'
+// ex. 'Dormitory' === 'dirty room##'
+
+function isAnagram(str1, str2) {}
+
+// CHALLENGE 5: LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+
+function letterChanges(str) {}
+
+// Call Function
+const output = longestWord("Hello, my name is Manuel");
+
+console.log(output);
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sample Courses From Traversy Media
 
-- Javascript Cardio [Session 1]
+- Javascript Cardio [Session 1, Session 2]
 - Node.js Crash Session
 - Async Js Session (Callbacks, Promises, Async Await)
 - JavaScript Higher Order Functions & Arrays
