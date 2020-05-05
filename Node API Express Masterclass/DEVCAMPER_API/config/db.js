aec0f59477a5b10e1969c0ff0cb99757933ac531
Manuel Rodriguez <manrodri92@gmail.com>
2020-05-06 00:45:59 +0300

    Added connection to MongoDB and mongoose

---
+++ Node API Express Masterclass/DEVCAMPER_API/config/db.js
@@ -0,0 +1,13 @@
+// Connection to Mongoose
+const mongoose = require('mongoose');
+const colors = require('colors');
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+module.exports = connectDB;
--- Node API Express Masterclass/DEVCAMPER_API/server.js
+++ Node API Express Masterclass/DEVCAMPER_API/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
 
 // Initialize app variable with Express
 const app = express();
@@ -20,9 +25,17 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `App listening on port ${PORT} running in ${process.env.NODE_ENV} mode`
+      .yellow.bold
   )
 );
+
+// Handle unhandled promises rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error ${err.message}`.red);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
