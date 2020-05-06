831b9ffd7cbd6fde1222340413e8bbbce9590008
Manuel Rodriguez <manrodri92@gmail.com>
2020-05-06 18:46:55 +0300

    Added: slugify middleware

--- Node API Express Masterclass/DEVCAMPER_API/models/Bootcamp.js
+++ Node API Express Masterclass/DEVCAMPER_API/models/Bootcamp.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const slugify = require('slugify');
+const slugify = require('slugify');
 // const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
@@ -113,4 +113,9 @@ const BootcampSchema = new mongoose.Schema(
   //   }
 );
 
+// Create bootcamp slug
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
