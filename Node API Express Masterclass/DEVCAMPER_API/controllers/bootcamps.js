693cfb24c254cdddf6b6005e398da95c5a704bbd
Manuel Rodriguez <manrodri92@gmail.com>
2020-05-06 20:33:10 +0300

    Added option to get bootcamp by radius

--- Node API Express Masterclass/DEVCAMPER_API/controllers/bootcamps.js
+++ Node API Express Masterclass/DEVCAMPER_API/controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const Bootcamp = require('../models/Bootcamp');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const ErrorResponse = require('../utils/errorResponse');
 // @desc   Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -64,4 +65,28 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
   res.status(200).json({ success: true, data: bootcamp });
+});
+
+// @desc   Get bootcamps within a radius
+// @route   DELETE /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  // Data come from URL
+  const { zipcode, distance } = req.params;
+
+  //    Get latitude and longitude from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  //   Calc radius using radians
+  // Divide distance by radius of earth
+  // Earth radius = 3,963 mi / 6,378
+  const radius = distance / 6378;
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+  res
+    .status(200)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
 });
--- Node API Express Masterclass/DEVCAMPER_API/routes/bootcamps.js
+++ Node API Express Masterclass/DEVCAMPER_API/routes/bootcamps.js
@@ -5,10 +5,12 @@ const {
   updateBootcamp,
   deleteBootcamp,
   createBootcamp,
+  getBootcampsInRadius,
 } = require('../controllers/bootcamps');
 const router = express.Router();
 
 // Routes come from the controllers
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootcamps).post(createBootcamp);
 router
   .route('/:id')
