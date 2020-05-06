517f632ffe9df83d049a35a4484d47416af54aa5
Manuel Rodriguez <manrodri92@gmail.com>
2020-05-06 23:54:10 +0300

    Added Course model & updated seeder

--- Node API Express Masterclass/DEVCAMPER_API/seeder.js
+++ Node API Express Masterclass/DEVCAMPER_API/seeder.js
@@ -8,7 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
-
+const Course = require('./models/Course');
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -22,10 +22,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data imported..'.green.inverse);
     process.exit();
   } catch (error) {
@@ -37,6 +42,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data Destroyed..'.red.inverse);
     process.exit();
   } catch (error) {
