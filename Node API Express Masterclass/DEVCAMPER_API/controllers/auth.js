454a045c3b625feffeb85b3c08ef475482cb7242
Manuel Rodriguez <manrodri92@gmail.com>
2020-05-10 10:49:24 +0300

    Added: User login and registration with JWT

---
+++ Node API Express Masterclass/DEVCAMPER_API/controllers/auth.js
@@ -0,0 +1,65 @@
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+
+// @desc   Register user
+// @route   POST /api/v1/auth/register
+// @access  Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  //   Create user
+  const user = await User.create({ name, email, password, role });
+
+  // Send token response
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc   Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse(`Please provide an email and password`, 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse(`Invalid credential`, 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse(`Invalid credential`, 401));
+  }
+
+  // Send token response
+  sendTokenResponse(user, 200, res);
+});
+
+// Get token from model, creates cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  //   Create Token
+  const token = user.getSignedJwtToken();
+  const options = {
+    expires: new Date(
+      // Calculates expiration for 30 days from now
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  //   Makes sure the cookies are sent over https in production mode
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};
--- Node API Express Masterclass/DEVCAMPER_API/server.js
+++ Node API Express Masterclass/DEVCAMPER_API/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
 const colors = require('colors');
+const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -16,12 +17,16 @@ connectDB();
 // route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 // Initialize app variable with Express
 const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 // Dev Logging Middleware
 if (process.env.NODE_ENV === 'development') {
@@ -37,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 // Error handler
 app.use(errorHandler);
