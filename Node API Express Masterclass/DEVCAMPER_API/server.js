ea9ca5f558dec4192cb333110396e2ba978c8a26
Manuel Rodriguez <manrodri92@gmail.com>
2020-05-13 19:03:45 +0300

    Added security to header and XSS attack prevention

--- Node API Express Masterclass/DEVCAMPER_API/server.js
+++ Node API Express Masterclass/DEVCAMPER_API/server.js
@@ -6,6 +6,8 @@ const fileUpload = require('express-fileupload');
 const colors = require('colors');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -41,6 +43,12 @@ app.use(fileUpload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security header
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
