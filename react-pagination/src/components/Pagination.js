c988d6bb1cd73c08e9c6a46484a0d286883e4f61
Manuel Rodriguez <manrodri92@gmail.com>
2021-01-12 12:34:51 +0300

    Finished simple pagination in React

---
+++ react-pagination/src/components/Pagination.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className='pagination'>
+        {pageNumbers.map((number) => (
+          <li key={number} className='page-item'>
+            <a href='!#' className='page-link' onClick={() => paginate(number)}>
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
